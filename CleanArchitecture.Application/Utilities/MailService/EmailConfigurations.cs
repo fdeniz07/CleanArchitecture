c6c85691b4b1ca6fde86a05b557b870f1243ba1d
Fatih Deniz <deniz.fatih@hotmail.de>
2023-11-04 21:45:02 +0100

    Day 07 - Mail Service

---
+++ CleanArchitecture.Application/Utilities/MailService/EmailConfigurations.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.Utilities.MailService
+{
+    public sealed record EmailConfigurations(
+        string Smtp,
+        string Password,
+        int Port,
+        bool SSL = true,
+        bool Html = true);
+}
