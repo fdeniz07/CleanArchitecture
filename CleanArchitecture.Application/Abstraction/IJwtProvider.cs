8d5201e6e305e29592100e5fc840e08c69dbf3a5
Fatih Deniz <deniz.fatih@hotmail.de>
2023-11-07 21:49:31 +0100

    Day 09 - Create Token

---
+++ CleanArchitecture.Application/Abstraction/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Abstraction
+{
+    public interface IJwtProvider
+    {
+        string CreateToken(User user);
+    }
+}
---
+++ CleanArchitecture.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CleanArchitecture.Application.Abstraction;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CleanArchitecture.Infrastructure.Authentication
+{
+    public sealed class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtProvider(IOptions<JwtOptions> jwtOptions) // Option Design Pattern kullanildigi icin burada sarmalliyoruz
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Name, user.UserName),
+                new Claim("FullName", user.FullName)
+            };
+
+            JwtSecurityToken jwtSecurityToken = new(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: null, notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)), SecurityAlgorithms.HmacSha256)
+            );
+
+            string token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return token;
+        }
+    }
+}
